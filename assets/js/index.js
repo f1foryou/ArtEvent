463d40ba8f26e485e4c9347f427bab1bf2fa994e
Aravind <aravinda.narayanan@xoanonanalytics.com>
2018-01-12 13:04:57 +0530

    Added Angular js Library

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,16 @@
 $( document ).ready(function(){
 });
+
+var mainApp = angular.module("mainApp", []);
+
+mainApp.controller('studentController', function($scope) {
+	$scope.student = {
+		firstName: "",
+		lastName: "",
+		fullName: function() {
+			var studentObject;
+			studentObject = $scope.student;
+			return studentObject.firstName + " " + studentObject.lastName;
+		}
+	};
+});
