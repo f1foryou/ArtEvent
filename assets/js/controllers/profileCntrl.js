5f1635b9284acf2b70210c81ded3ef674eeae672
Aravind <aravinda.narayanan@xoanonanalytics.com>
2018-01-30 17:52:12 +0530

    Added Pricing Plan in home page

--- assets/js/controllers/profileCntrl.js
+++ assets/js/controllers/profileCntrl.js
@@ -4,7 +4,15 @@ define(['./module'], function (controllers) {
 		$scope.event = [
 			{"id":1,"name":"Event 1", "end_date":"Sep 10 2018","img":"assets/images/1000x400.png"},
 		];
-
+		$scope.ongoingEvents=[
+			{"id":1,"name":"Event 1", "end_date":"Sep 10 2018","img":"assets/images/1000x400.png"},
+			{"id":2,"name":"Event 2", "end_date":"Oct 10 2018","img":"assets/images/1000x400.png"},
+			{"id":3,"name":"Event 3", "end_date":"Nov 10 2018","img":"assets/images/1000x400.png"},
+		];
+		$scope.participatedEvents=[
+			{"id":1,"name":"Event 1", "end_date":"Sep 10 2018","img":"assets/images/1000x400.png"},
+			{"id":2,"name":"Event 2", "end_date":"Oct 10 2018","img":"assets/images/1000x400.png"},
+		];
 		return $scope;
 	}]);
 });
